5021412b2802e4ca7ba3221a93af87dca84d6b26
wangwen135 <wangwen135@gmail.com>
2015-05-06 16:10:34 +0800

    JDBC API 测试

--- src/com/ww/jdbcTest/JDBCTest.java
+++ src/com/ww/jdbcTest/JDBCTest.java
@@ -48,6 +48,7 @@ public class JDBCTest {
 		Class.forName(DRIVE);
 
 		Enumeration<Driver> erd = DriverManager.getDrivers();
+		System.out.println("获取带有当前调用者可以访问的所有当前已加载 JDBC 驱动程序的:");
 		/**
 		 * 包含了两个 sun.jdbc.odbc.JdbcOdbcDriver@8813f2
 		 * oracle.jdbc.driver.OracleDriver@1d58aae
@@ -69,36 +70,88 @@ public class JDBCTest {
 		// JDBC 日志流默认为空
 
 		// 构建一个新的
-		PrintWriter printWriter = new PrintWriter(System.out);
+		PrintWriter printWriter = new PrintWriter(System.err);
 
 		// 设置由 DriverManager 和所有驱动程序使用的日志/追踪 PrintWriter 对象。
 		DriverManager.setLogWriter(printWriter);
 
 		DriverManager.println("### 测 试 信 息 ### DriverManager.setLogWriter");
+		DriverManager.println("设置日志为系统错误输出");
 
 		conn = DriverManager.getConnection(URL, DBUSER, PASSWORD);
 
-		System.out.println("获取的连接对象是：" + conn);
-		System.out.println("conn.getAutoCommit() = " + conn.getAutoCommit());
-		System.out.println("conn.getCatalog() = " + conn.getCatalog());
-		
+		System.out.println("\n\n获取的连接对象是：" + conn);
+		System.out
+				.println("获取此 Connection 对象的当前自动提交模式 " + conn.getAutoCommit());
+		System.out.println("获取此 Connection 对象的当前目录名称 " + conn.getCatalog());
+		// System.out.println(conn.getClientInfo());
+		/**
+		 * <pre>
+		 *  java.sql.Connection 
+		 *  public static final int TRANSACTION_NONE 0 
+		 *  public static final int TRANSACTION_READ_COMMITTED 2 
+		 *  public static final int TRANSACTION_READ_UNCOMMITTED 1 
+		 *  public static final int TRANSACTION_REPEATABLE_READ 4 
+		 *  public static final int TRANSACTION_SERIALIZABLE 8
+		 * </pre>
+		 */
+		System.out.println("获取此 Connection 对象的当前事务隔离级别:"
+				+ conn.getTransactionIsolation());
 
 		/**
 		 * Connection 对象的数据库能够提供描述其表、所支持的 SQL 语法、存储过程、此连接功能等等的信息。此信息是使用
 		 * getMetaData 方法获得的。
 		 */
+		System.out.println("\n\nconn.getMetaData()\n");
 		DatabaseMetaData condmd = conn.getMetaData();
+
+		System.out.println("数据库名:" + condmd.getDatabaseProductName());
+
+		System.out.println("数据库版本号:" + condmd.getDatabaseProductVersion());
+
+		System.out.println("驱动名:" + condmd.getDriverName());
+
+		System.out.println("驱动版本号:" + condmd.getDriverVersion());
+
+		System.out.println("是否支持事务:" + condmd.supportsTransactions());
+
+		System.out.println("默认事务隔离级别:"
+				+ condmd.getDefaultTransactionIsolation());
+
+		System.out.println("是否支持指定的事务隔离级别:"
+				+ condmd.supportsTransactionIsolationLevel(0));
+
+		System.out.println("是否支持获取主键:" + condmd.supportsGetGeneratedKeys());
+
+		System.out
+				.println("是否支持敏感结果集"
+						+ condmd.supportsResultSetType(ResultSet.TYPE_SCROLL_SENSITIVE));
+
+		System.out.println("\n\n");
 
 		stmt = conn.createStatement();
 
 		// 执行SQL语句来查询数据库
 
-		result = stmt.executeQuery("select * from AA_T1");
+		String sql = "select * from AA_T1";
+		result = stmt.executeQuery(sql);
+		System.out.println("执行SQL:" + sql);
 
 		ResultSetMetaData rsmd = result.getMetaData();
 
 		// 获得结果集列数
 		int columnCount = rsmd.getColumnCount();
+		System.out.println("结果集中包含的列数： " + columnCount);
+
+		for (int i = 1; i <= columnCount; i++) {
+			System.out.println("列：" + i + " 列名称：" + rsmd.getColumnName(i)
+					+ " 列别名：" + rsmd.getColumnLabel(i) + " 数据库类型："
+					+ rsmd.getColumnTypeName(i) + " Java类型："
+					+ rsmd.getColumnClassName(i));
+		}
+
+		System.out.println("\n查询结果：\n");
+
 		while (result.next()) {// 判断有没有下一行
 
 			Map<String, Object> map = new HashMap<String, Object>();
