6855da9775a00f03bbdb4755839da5458b982028
ww <wangwen135@gmail.com>
2015-11-18 17:13:37 +0800

    大 阶乘 和 次方

---
+++ src/main/java/com/wwh/match/Factorial.java
@@ -0,0 +1,45 @@
+package com.wwh.match;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+/**
+ * <pre>
+ * 阶乘
+ * </pre>
+ *
+ * @author wwh
+ * @date 2015年11月18日 下午4:12:43
+ *
+ */
+public class Factorial {
+    public static BigDecimal factorial(int n) {
+        BigDecimal result = new BigDecimal(1);
+        BigDecimal a;
+        for (int i = 2; i <= n; i++) {
+            a = new BigDecimal(i);// 将i转换为BigDecimal类型
+            result = result.multiply(a);// 不用result*a，因为BigDecimal类型没有定义*操作</span><span>
+        }
+        return result;
+    }
+
+    public static void main(String[] arguments) {
+        System.out.println("输入要计算阶乘的数字：");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();// 读取控制台输入的整数
+        sc.close();
+        BigDecimal result = factorial(a);
+
+        int precision = result.precision();
+        System.out.println(a + " 的阶乘");
+        System.out.println("其结果精度是：" + precision);
+
+        if (precision > 1000) {
+            System.out.println("太大了...");
+        } else {
+            String str = result.toEngineeringString();
+            System.out.println(str);
+        }
+
+    }
+}
---
+++ src/main/java/com/wwh/match/Power.java
@@ -0,0 +1,51 @@
+package com.wwh.match;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+/**
+ * <pre>
+ * 超大号次方
+ * </pre>
+ *
+ * @author wwh
+ * @date 2015年11月18日 下午4:33:01
+ *
+ */
+public class Power {
+
+    public static BigDecimal power(int a, int b) {
+        BigDecimal result = BigDecimal.ONE;
+        if (b <= 0) {
+            return BigDecimal.ONE;
+        } else {
+            BigDecimal A = new BigDecimal(a);
+            for (int i = 0; i < b; i++) {
+                result = result.multiply(A);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] arguments) {
+        System.out.println("输入数字：");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();// 读取控制台输入的整数
+        System.out.println("输入次方：");
+        int b = sc.nextInt();
+        sc.close();
+        BigDecimal result = power(a, b);
+
+        int precision = result.precision();
+        System.out.println(a + " 的 " + b + " 次方");
+        System.out.println("其结果精度是：" + precision);
+
+        if (precision > 1000) {
+            System.out.println("太大了...");
+        } else {
+            String str = result.toEngineeringString();
+            System.out.println(str);
+        }
+
+    }
+}
