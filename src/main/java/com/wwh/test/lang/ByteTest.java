786787fef10e5d0d9d7b8905ee7f634892ffdaed
wangwenhua <wangwh@vvsmart.com>
2020-05-18 15:16:42 +0800

    拼写错误

--- src/main/java/com/wwh/test/lang/ByteTest.java
+++ src/main/java/com/wwh/test/lang/ByteTest.java
@@ -3,6 +3,24 @@ package com.wwh.test.lang;
 public class ByteTest {
 
     public static void main(String[] args) {
+        System.out.println("Byte类型最大值：" + Byte.MAX_VALUE);// 127
+        System.out.println("Byte类型最小值：" + Byte.MIN_VALUE);// -128
+
+        // 转成无符号的int
+        int unsignedByte = Byte.toUnsignedInt(Byte.MIN_VALUE);
+        System.out.println(unsignedByte); // 128
+
+        int b1 = 0b10001000;
+        System.out.println(b1); // 136
+
+        int i1 = 0xFF;
+        System.out.println(i1);// 255
+
+        int i2 = 0xFFFF; // 65535
+        System.out.println(i2);
         
+        byte b = 12;
+        System.out.println(Byte.toString(b));
+
     }
 }
